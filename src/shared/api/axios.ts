277b8757b16f5383490230b40f92f7e83347a5e7
Артём Ковалев <artiomkratov@gmail.com>
2025-12-26 23:08:37 +0300

    fix: fixed URL

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v0';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api/v0';
 
 const apiClient = axios.create({
     baseURL: API_URL,
